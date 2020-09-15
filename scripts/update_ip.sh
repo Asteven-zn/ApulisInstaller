95a77e931dd8104d657ed686b974e0cd5f2a0c24
KMFtcy <kmftangchaoyang@gmail.com>
2020-09-15 12:36:44 +0800

    finish update ip script

--- scripts/update_ip.sh
+++ scripts/update_ip.sh
@@ -1,9 +1,51 @@
+DLWS_HOME="/home/dlwsadmin"
+INSTALLED_DIR="/home/dlwsadmin/DLWorkspace"
+DLWS_CONFIG_DIR="${INSTALLED_DIR}/YTung/src/ClusterBootstrap"
 ############################################################################
 #
 #   MAIN CODE START FROM HERE
 #
 ############################################################################
+echo'
+* Notice *
+1. Please make sure /etc/hosts has been updated.
+2. Please make sure ./config/install_config.json exists, and has been updated.
+* Notice *
+Press any [Enter] to continue >>>
+'
+read -r dump
+# reset cluster
 yes | kubeadm reset
+# reset config.yaml
+cd ${DLWS_CONFIG_DIR}
+master_hostname=`hostname`
+for hostname in `cat config.yaml | grep " role: infrastructure" -B 1 |  grep -v "infrastructure" | sed "s/\://" | grep -v "^--"`
+do
+    host_ip=`grep "${hostname}" /etc/hosts | grep -v 127 | grep -v ${hostname}\. | awk '{print $1}'`
+    if [[ ${host_ip == "" }]]
+    then
+        printf "!!! Can't find node %s in /etc/hosts !!!\n" ${hostname}
+        printf " Please make sure everything is ready and relaunch again. \n"
+        exit
+    fi
+    # grep double times, so that it can save space before "private: xxx"
+    new_config=`grep "${hostname}:" config.yaml -A 2 | grep "private-ip:" | sed "s/: .*/: ${host_ip}/g"`
+    origin_config=`grep "${hostname}:" config.yaml -A 2 | grep "private-ip:" `
+    sed "s/${origin_config}/${new_config}/g" -i config.yaml
+done
+
+cd -
+if [ ! -f "config/install_config.json" ]
+then
+    printf "!!! Can't find config json file !!!\n" ${hostname}
+    printf " Please make sure everything is ready and relaunch again. \n"
+    exit
+fi
+new-kube-vip=`cat config/install_config.json.template | grep kube_vip | sed "s?\"??g" | sed "s?.*\:??g"`
+cd ${DLWS_CONFIG_DIR}
+sed "s|kube-vip:.*|kube-vip: ${new-kube-vip}|g" -i config.yaml
+
+# deploy cluster again
 ./deploy.py --verbose kubeadm init ha
 ./deploy.py --verbose copytoall ./deploy/sshkey/admin.conf /root/.kube/config
 
