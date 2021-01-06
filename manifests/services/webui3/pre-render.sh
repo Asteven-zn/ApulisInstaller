8b211267de27e6dfb286d7eeee736a4eb0af4b45
dingbai.liao <dingbai.liao@apulis.com>
2021-01-06 17:09:39 +0800

    feat(webui): add webui configmap

---
+++ manifests/services/webui3/pre-render.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir=`dirname $0`
+
+webui_conf_yaml=${dir}/01.webui-cm.yaml
+
+{{ bin_dir }}/kubectl --namespace=kube-system create configmap webui-cm \
+  --from-file=${dir}/configAuth.json \
+  --from-file=${dir}/hosting.json \
+  --from-file=${dir}/local.yaml \
+  --from-file=${dir}/Master-Templates.json \
+  --from-file=${dir}/userconfig.json \
+  --dry-run=client -o yaml > $webui_conf_yaml
