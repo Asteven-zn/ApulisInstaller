14254e406e929352d7f474ad71dfb5dc05a863ce
dingbai.liao <dingbai.liao@apulis.com>
2021-01-08 15:08:33 +0800

    fix(istio): shell typo

--- manifests/services/istio/pre-render.sh
+++ manifests/services/istio/pre-render.sh
@@ -2,7 +2,7 @@
 
 dir=`dirname $0`
 
-if [ '{{service["use_service"]"}}' == "istio" ];
+if [ "{{service["use_service"]}}" == "istio" ];
 then
     {{ bin_dir }}/istioctl install -f ${dir}/istio.yaml --set values.global.jwtPolicy=first-party-jwt --force
 fi
