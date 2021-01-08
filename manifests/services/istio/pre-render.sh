979bab031870031862d57423f1fc2895f5759705
dingbai.liao <dingbai.liao@apulis.com>
2021-01-08 14:49:21 +0800

    fix(istio): fix istio service and knative

--- manifests/services/istio/pre-render.sh
+++ manifests/services/istio/pre-render.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [ "$use_service" = "istio" ];
+dir=`dirname $0`
+
+if [ '{{service["use_service"]"}}' == "istio" ];
 then
-    if [ "$(uname -m)" = "aarch64" ]; then
-      {{ bin_dir }}/istioctl install -f deploy/services/istio/istio-arm64.yaml --set values.global.jwtPolicy=first-party-jwt --force
-    else
-      {{ bin_dir }}/istioctl install -f deploy/services/istio/istio.yaml --set values.global.jwtPolicy=first-party-jwt --force
-    fi
+    {{ bin_dir }}/istioctl install -f ${dir}/istio.yaml --set values.global.jwtPolicy=first-party-jwt --force
 fi
