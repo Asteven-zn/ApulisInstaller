787bd4e1425623b7428bf222f502d1dbf08bc75a
dingbai.liao <dingbai.liao@apulis.com>
2021-01-11 17:40:07 +0800

    feat(appsetting): add data platform appsettings config

--- manifests/services/restfulapi2/pre-render.sh
+++ manifests/services/restfulapi2/pre-render.sh
@@ -5,5 +5,5 @@ dir=`dirname $0`
 config_file=${dir}/01.restful-cm.yaml
 
 {{ bin_dir }}/kubectl create configmap restful-cm --from-file=${dir}/config.yaml \
-  --from-file=${dir}/appsettings.json
+  --from-file=${dir}/appsettings.json \
   --dry-run -o yaml > $config_file
